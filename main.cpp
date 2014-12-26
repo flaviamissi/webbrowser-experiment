befd806b0cf03f2863fe2f3f581decc433197613
Flavia Missi <flaviamissi@gmail.com>
2014-12-26 11:09:14 -0200

    built class to wrap up http requests, only supports get for now

--- main.cpp
+++ main.cpp
@@ -1,18 +1,66 @@
 #include <curl/curl.h>
 #include <iostream>
 
+// libcurl write callback function
+static int writer(char *data, size_t size, size_t nmemb, std::string *writer_data) {
+    if (writer_data == NULL)
+        return 0;
+    writer_data->append(data, size * nmemb);
+    return size * nmemb;
+}
 
-int main() {
-    curl_global_init(CURL_GLOBAL_ALL);
+class Request {
+    CURL *handle;
+    char error_buffer[CURL_ERROR_SIZE];
+    std::string data_buffer;
+    public:
+        Request();
+        std::string get(std::string);
+};
 
-    CURL *handle = curl_easy_init();
+Request::Request() {
+    handle = curl_easy_init();
+    CURLcode code;
 
-    curl_easy_setopt(handle, CURLOPT_URL, "http://flaviamissi.com.br");
+    code = curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, error_buffer);
+    if (code != CURLE_OK) {
+        std::cerr << "Failed to set error buffer " << code << std::endl;
+    }
+
     curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+    if (code != CURLE_OK) {
+        std::cerr << "Failed to set redirect option " << error_buffer << std::endl;
+    }
+
+    code = curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer);
+    if (code != CURLE_OK) {
+        std::cerr << "Failed to set writer " << error_buffer << std::endl;
+    }
+
+    code = curl_easy_setopt(handle, CURLOPT_WRITEDATA, &data_buffer);
+    if (code != CURLE_OK) {
+        std::cerr << "Failed to set write data " << error_buffer << std::endl;
+    }
+}
+
+std::string Request::get(std::string url) {
+    curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
 
     CURLcode resp = curl_easy_perform(handle);
     if(resp != CURLE_OK)
         std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(resp) << std::endl;
+
+    return data_buffer;
+}
+
+int main() {
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    Request request;
+    std::string result = request.get("http://flaviamissi.com.br");
+
+    std::cout << "result html is: " << std::endl;
+    std::cout << result << std::endl;
 
     curl_global_cleanup();
     return 0;
